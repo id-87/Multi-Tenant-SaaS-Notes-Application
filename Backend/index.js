27d56d56df11207fa2e2d980796f2cc3936de7a0
Harshit Gupta <hrstgpt87@gmail.com>
2025-09-25 18:13:55 +0530

    cors fix

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,27 @@ const cors = require("cors")
 const app=express()
 
 app.use(express.json())
-app.use(cors({
-    origin : "*",
-    methods : ["GET","POST","PUT","DELETE"],
+
+
+const allowedOrigins = ['https://multi-tenant-saa-s-notes-applicatio-lovat-two.vercel.app/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
+
+
+// app.use(cors({
+//     origin : "*",
+//     methods : ["GET","POST","PUT","DELETE"],
     
-}))
+// }))
 
 
 app.get("/health",(req,res)=>{
