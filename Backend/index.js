2231288df35cf3b61ed6b5c17699c327d44f8aab
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2025-09-25 16:44:59 +0530

    CORS setup

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,18 @@ const express=require("express")
 const {PrismaClient}=require("@prisma/client")
 const notesRouter=require("./routes/notesRoutes")
 const authRoutes=require("./routes/authRoutes")
-
+const cors = require("cors")
 
 const app=express()
 
 app.use(express.json())
+app.use(cors({
+    origin : "*",
+    methods : ["GET","POST","PUT","DELETE"],
+    
+}))
+
+
 app.get("/health",(req,res)=>{
     res.send("ok")
 })
